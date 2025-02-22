85e0373fcd7c80ba2edf49477f09e90d7ec02fb7
Mahi <mahichowdhury587@gmail.com>
2025-02-22 18:07:29 +0600

    feat: add analytics page

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        Nunito: ["Nunito", "sans-serif"]
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
