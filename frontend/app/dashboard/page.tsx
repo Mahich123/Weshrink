3af1bb314eb21e79f9c2a1e3a0b0ff164bbbc514
Mahi <mahichowdhury587@gmail.com>
2025-02-23 22:22:13 +0600

    fix: urlList page

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,14 +1,15 @@
-"use client"
 
 import Header from "@/components/Header"
 import CreateUrlForm from "@/components/CreateUrlForm"
+import UrlList from "@/components/UrlList"
 
 export default function DashBoardPage() {
 
     return (
-        <div className="bg-gradient-radial from-[#4E4E55] to-[#151A36] min-h-screen">
+        <div className="bg-gradient-radial from-[#4E4E55] to-[#151A36] font-Nunito min-h-screen">
             <Header />
             <CreateUrlForm />
+            <UrlList />
         </div>
     )
 }
---
+++ frontend/components/UrlList.tsx
@@ -0,0 +1,145 @@
+"use server"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { client } from "@/utils/honoClient";
+import {
+    Copy,
+    ChevronLeft,
+    ChevronRight,
+    BarChart2,
+    Calendar,
+    ExternalLink,
+} from "lucide-react";
+import { headers } from "next/headers";
+import { CopyButton } from "@/components/CopyButton";
+
+export default async function UrlList() {
+    let urls:
+        | {
+            id: number;
+            name: string | null;
+            longUrl: string;
+            userID: string | null;
+            alias: string | null;
+            short: string;
+            expiresAt: string;
+            expired: boolean;
+            clickCount: number;
+            createdAt: string;
+        }[]
+        | null = null;
+
+    try {
+        const res = await client.urls.$get();
+
+        if (res.ok) {
+            urls = (await res.json())?.urls;
+            console.log(urls);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+    const headersList = headers();
+    const host = (await headersList).get("host");
+    const protocol = (await headersList).get("x-forwarded-proto") || "http";
+
+    const fullUrl = `${protocol}://${host}/`;
+
+    return (
+        <div className="min-h-screen text-white p-4  md:p-16 font-Nunito">
+            <div className="max-w-7xl mx-auto space-y-8">
+                <div className="flex justify-center items-center relative w-full">
+                    <Input
+                        className="w-2/6 bg-[#6E6F77] border border-[#F4F4F4] h-12 rounded-xl outline-none text-center text-xl placeholder:text-white placeholder-opacity-100"
+                        placeholder="Search your desired links"
+                    />
+
+
+                </div>
+
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {urls?.map((url, i) => (
+                        <div
+                            key={i}
+                            className="bg-[#60567866] border-[1px] rounded-lg p-6 space-y-4 backdrop-blur-sm"
+                        >
+                            <div className="flex justify-between items-center border-b-[1px]">
+                                <h3 className="text-3xl py-2 font-semibold">
+                                    {url.name || "Untitled"}
+                                </h3>
+                                <CopyButton text={url.name || "Untitled"}>
+                                    <Copy className="h-4 w-4" />
+                                </CopyButton>
+                            </div>
+
+                            <div className="space-y-1">
+                                <div className="pb-2">Long URL</div>
+                                <div className="flex gap-2">
+                                    <Input
+                                        value={url.longUrl}
+                                        readOnly
+                                        className="border-[1px]"
+                                    />
+                                    <CopyButton text={url.longUrl}>
+                                        <ExternalLink className="h-4 w-4" />
+                                    </CopyButton>
+                                </div>
+                            </div>
+
+                            <div className="space-y-1">
+                                <div className="pb-2">Short URL</div>
+                                <div className="flex gap-2">
+                                    <Input
+                                        value={fullUrl + url.short}
+                                        readOnly
+                                        className="border-[1px]"
+                                    />
+                                    <CopyButton text={fullUrl + url.short}>
+                                        <ExternalLink className="h-4 w-4" />
+                                    </CopyButton>
+                                </div>
+                            </div>
+
+                            <div className="flex flex-row gap-2 items-center">
+                                <Calendar className="h-4 w-4" />
+                                Expires on: {url.expiresAt}
+                            </div>
+
+                            <div className="flex gap-4 pt-2">
+                                <Button variant="ghost" className="flex-1 border-[1px]">
+                                    <BarChart2 className="h-4 w-4 mr-2" />
+                                    Check Stats
+                                </Button>
+                                <Button variant="ghost" className="flex-1 border-[1px]">
+                                    Update Link
+                                </Button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                {/* Pagination */}
+                <div className="flex justify-center items-center gap-2">
+                    <Button variant="ghost" size="icon">
+                        <ChevronLeft className="h-4 w-4" />
+                    </Button>
+                    {[1, 2, 3, 4].map((page) => (
+                        <Button
+                            key={page}
+                            size="icon"
+                            className={`bg-transparent
+                   ${page === 1 ? "rounded-full border-2" : ""}`}
+                        >
+                            {page}
+                        </Button>
+                    ))}
+                    <Button variant="ghost" size="icon">
+                        <ChevronRight className="h-4 w-4" />
+                    </Button>
+                </div>
+            </div>
+        </div>
+
+    );
+}
